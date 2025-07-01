965ed9f72fb13cefe1355a6fac0db7e0127bdb26
ruzica-glavas <ruzi99@hotmail.it>
2025-07-01 11:08:44 +0200

    add: routes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import GlobalContext from "./contexts/GlobalContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefualtLayout from "./layouts/DefaultLayout";
 
 export default function App() {
+
+    const [isLoading, setIsLoading] = useState(false);
+
     return(
-    <Header/>
+    <GlobalContext.Provider value={{isLoading, setIsLoading}}>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<DefualtLayout/>}>
+                    <Route index path="/"/>
+                    <Route path="/vehicles/:id"/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </GlobalContext.Provider>
+
     )
 }
 
